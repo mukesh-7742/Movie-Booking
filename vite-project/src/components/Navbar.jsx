777b22ee2f6ce4e6ac0964ff856667f8bd00c183
mukesh-7742 <mukeshkthakur29@gmail.com>
2025-07-24 13:54:26 +0530

    2 commit and upadate

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
             className="block px-4 py-2 rounded hover:bg-indigo-100 text-indigo-700 font-medium"
             onClick={() => setOpen(false)}
           >
-             Booking 
+             My Booking 
           </Link>
         </li>
         <li className="mx-0 md:mx-3 my-2 md:my-0">
